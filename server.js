f187f9079c2aeecd8c1299539fe7e7673b2e4540
Zghinnyk Yevhen <zzginnik@gmail.com>
2022-06-07 17:07:03 +0300

    added work with files

--- server.js
+++ server.js
@@ -1,44 +1,121 @@
 const http = require('http');
 const fs = require('fs');
 
-const delay = (ms) => {
+// const delay = (ms) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve()
+//     }, ms)
+//   })
+// } 
+
+// const readFile = (path) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(path, (err, data) => {
+//       if(err) {
+//         reject(err);
+//       };
+//       resolve(data);
+//     });
+//   })
+// } 
+
+const getBody = (req) => {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, ms)
+    let body = '';
+    req.on('data', function (data) {
+      if(!data) {
+        return;
+      }
+      body += data;
+    });
+    req.on('end', function () {
+      const parsed = JSON.parse(body);
+      resolve(parsed);
+    });
+  })
+}
+
+const existFile = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.exists(path, (isExist) => {
+      resolve(isExist)
+    })
   })
 } 
 
-const readFile = (path) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, (err, data) => {
-      if(err) {
-        reject(err);
-      };
-      resolve(data);
-    });
+const writeData = (path, data) => {
+  return new Promise((resolve) => {
+    const writeStream = fs.createWriteStream(path);
+    writeStream.write(data);
+    resolve(data);
+    writeStream.end();
   })
-} 
+}
+
+const postFile = async (req, res) => {
+  try {
+    const body = await getBody(req);
+    if(!body?.fileName || !body?.data) {
+      res.write(`Your request body is not completed!!!`);
+      res.end();
+      return;
+    }
+    const response = await existFile(`./db/${body.fileName}.txt`);
+    if(response) {
+      const readStream = fs.createReadStream(`./db/${body.fileName}.txt`);
+      readStream.on('data', chunk => {
+        if(!chunk) {
+          return;
+        }
+        writeData(`./db/${body.fileName}.txt`, chunk + body.data);
+      });
+      readStream.on('end', () => {
+        res.end();
+      });
+    } else {
+      await writeData(`./db/${body.fileName}.txt`, body.data);
+      res.end();
+    }
+  } catch {
+    console.log('ERROR');
+  }
+}
+
+const deleteFile = async (fileName, res) => {
+  try {
+    const response = await existFile(`./db/${fileName}.txt`);
+    if(response) {
+      fs.unlink( `./db/${fileName}.txt`, () => {
+        res.write(fileName + ' DELETED');
+        res.end();
+      })
+    } else {
+      res.write(fileName + '.txt' + ' not found');
+      res.end();
+    }
+  } catch {
+  }
+}
 
 const server = http.createServer(async (
   request, response
 ) => {
-  switch(request.url) {
-    case '/home' :
-      await delay(1000);
-      const firstPage = await readFile('./pages/first.html');
-      response.write(firstPage);
-      response.end();
-      break;
-    case '/about' :
-      await delay(3000);
-      const aboutPage = await readFile('./pages/about.html');
-      response.write(aboutPage);
-      response.end();
+  const parsedUrl = request.url.split('/');
+  switch(parsedUrl[1]) {
+    case 'write' :
+      if(request.method === 'GET') {
+        
+      } else if(request.method === 'POST') {
+        await postFile(request, response);
+      } else if(request.method === 'PATCH') {
+      } else if(request.method === 'DELETE') {
+        await deleteFile(parsedUrl[2], response);
+      }
       break;
     default :
       response.write(`ERROR 404 ${request.url} is unknown`);
-      response.end();
+      response.end('ERROR 404');
   }
 })
 

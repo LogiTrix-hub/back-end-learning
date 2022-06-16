5958d2e7e09dbd79fd57ea050ca8361d2be9fe51
Zghinnyk Yevhen <zzginnik@gmail.com>
2022-06-16 17:48:39 +0300

    init commit for first express ver

--- dist/index.js
+++ dist/index.js
@@ -5,23 +5,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const express_handlebars_1 = require("express-handlebars");
 const app = (0, express_1.default)();
 // ToDo: remove row below when will start working with cors policy
 app.use((0, cors_1.default)());
-const port = 3006;
-app.get("/", (req, res) => {
-    const a = 4;
-    if (a > 5) {
-        res.send("OK!");
-    }
-    else {
-        res.send("Hello World!SADSADSAD");
-    }
+const fortunes = [
+    "Победи свои страхи, или они победят тебя.",
+    "Рекам нужны истоки.",
+    "Не бойся неведомого.",
+    "Тебя ждет приятный сюрприз.",
+    "Будь проще везде, где только можно.",
+];
+const port = process.env.PORT || 3006;
+app.engine("handlebars", (0, express_handlebars_1.engine)({
+    defaultLayout: "main",
+}));
+app.set("view engine", "handlebars");
+app.use(express_1.default.static("public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/about", (_, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render("about", { fortune: randomFortune });
 });
-app.get("/users", (req, res) => {
-    res.send({
-        users: ["john", "john", "john", "john", "john", "john"],
-    });
+// Пользовательская страница 404
+app.use((_, res) => {
+    res.type("text/plain");
+    res.render("404");
+});
+// Пользовательская страница 500
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500);
+    res.render("500");
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,48 @@
 import express from "express";
 import cors from "cors";
+import { engine } from "express-handlebars";
 
 const app = express();
 // ToDo: remove row below when will start working with cors policy
 app.use(cors());
-const port = 3006;
 
-app.get("/", (req, res) => {
-  const a = 4;
-  if (a > 5) {
-    res.send("OK!");
-  } else {
-    res.send("Hello World!SADSADSAD");
-  }
+const fortunes = [
+  "Победи свои страхи, или они победят тебя.",
+  "Рекам нужны истоки.",
+  "Не бойся неведомого.",
+  "Тебя ждет приятный сюрприз.",
+  "Будь проще везде, где только можно.",
+];
+
+const port = process.env.PORT || 3006;
+
+app.engine(
+  "handlebars",
+  engine({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (_, res) => res.render("home"));
+app.get("/about", (_, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render("about", { fortune: randomFortune });
 });
 
-app.get("/users", (req, res) => {
-  res.send({
-    users: ["john", "john", "john", "john", "john", "john"],
-  });
+// Пользовательская страница 404
+app.use((_, res) => {
+  res.type("text/plain");
+  res.render("404");
+});
+
+// Пользовательская страница 500
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500);
+  res.render("500");
 });
 
 app.listen(port, () => {
